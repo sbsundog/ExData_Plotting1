37918cca3b0951271a4fae1c871461e0d4a6a4a8
John Emerick <jaemerick1@cox.net>
2016-01-09 00:12:23 -0800

    First commit

---
+++ plot2.R
@@ -0,0 +1,38 @@
+##    This script is plot2.R. 
+##    It creates plot2.png
+##
+##    This R script utilizes a common R script for acquiring, preparing and oresenting household power
+##    consumption data for February 1 and 2, 2007. The plot created by the pre-processing is described
+##    in subsequent comments.
+##    
+##    
+##
+##    
+##
+dir.create("c:/Users/John/Documents", showWarnings = FALSE, recursive = TRUE)   # Create all elements on the path 
+# Or report nothing if the path exists
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip" # Project data Url
+
+download.file(fileUrl, dest = "c:/Users/John/Documents/hpc.zip", mode = "wb")    # Place project data on local drive
+unzip("c:/Users/John/Documents/hpc.zip", exdir = "c:/Users/John/Documents")      # Create Household_power_consumption.txt from unzip
+
+column.classes <- c(rep("character", 2), rep("numeric", 7))                      # identify column classes since otherwise factors abound
+electric.data <- read.table("c:/Users/John/Documents/household_power_consumption.txt", sep=";", header = FALSE,
+                            na.strings ="?", skip = 1, colClasses = column.classes)      # electric data contains all complete rows
+#Provide names to the Variables
+colnames (electric.data) <- c("Date", "Time", "Global.Active.Power", 
+                              "Global.Reactive.Power", "Voltage", "Global.Intensity", "Sub.Metering.1", "Sub.Metering.2", "Sub.Metering.3")
+
+j12 <- subset(electric.data, Date %in% c("1/2/2007", "2/2/2007"))                #Reduce data to two dates as specified in Assignment directions 
+date.time <- paste(j12$Date,j12$Time, sep=" ")                                   #Prepare date time 
+date.time <- strptime(date.time, "%d/%m/%Y %H:%M:%S")                            #End of common script for Assignnment plots
+##
+##
+##                        
+##PLOT 2 Detail                                                                  #Date.Time vs Global ACtive Power plot
+##
+png("c:/Users/John/Documents/plot2.png", width = 480, height = 480)
+par(mfrow=c(1,1))
+with(j12, plot(date.time, j12$Global.Active.Power, type = "l", ylab = "Global Active Power (kilowatts)", xlab =""))
+dev.off()
+##
---
+++ plot4.R
@@ -0,0 +1,44 @@
+##    This script is plot4.R. 
+##    It creates plot4.png
+##
+##    This R script utilizes a common R script for acquiring, preparing and oresenting household power
+##    consumption data for February 1 and 2, 2007. The plot created by the pre-processing is described
+##    in subsequent comments.
+##    
+##    
+##
+##    
+##
+dir.create("c:/Users/John/Documents", showWarnings = FALSE, recursive = TRUE)   # Create all elements on the path 
+# Or report nothing if the path exists
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip" # Project data Url
+
+download.file(fileUrl, dest = "c:/Users/John/Documents/hpc.zip", mode = "wb")    # Place project data on local drive
+unzip("c:/Users/John/Documents/hpc.zip", exdir = "c:/Users/John/Documents")      # Create Household_power_consumption.txt from unzip
+
+column.classes <- c(rep("character", 2), rep("numeric", 7))                      # identify column classes since otherwise factors abound
+electric.data <- read.table("c:/Users/John/Documents/household_power_consumption.txt", sep=";", header = FALSE,
+                            na.strings ="?", skip = 1, colClasses = column.classes)      # electric data contains all complete rows
+#Provide names to the Variables
+colnames (electric.data) <- c("Date", "Time", "Global.Active.Power", 
+                              "Global.Reactive.Power", "Voltage", "Global.Intensity", "Sub.Metering.1", "Sub.Metering.2", "Sub.Metering.3")
+
+j12 <- subset(electric.data, Date %in% c("1/2/2007", "2/2/2007"))                #Reduce data to two dates as specified in Assignment directions 
+date.time <- paste(j12$Date,j12$Time, sep=" ")                                   #Prepare date time 
+date.time <- strptime(date.time, "%d/%m/%Y %H:%M:%S")                            #End of common script for Assignnment plots
+##
+##
+## Plot 4 Detail                                                                        Four plots 2 by 2
+##
+png("c:/Users/John/Documents/plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+plot(date.time, j12$Global.Active.Power, type = "l", ylab = "Global Active Power", xlab = "") 
+plot(date.time, j12$Voltage, type = "l", ylab = "Voltage", xlab = "datetime") 
+plot(date.time, j12$Sub.Metering.1, type = "l", ylab = "Energy sub metering", xlab = "", col = "black")
+legend("topright", pch = "__", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+lines(date.time, j12$Sub.Metering.2, col = "red")
+lines(date.time, j12$Sub.Metering.3, col = "blue")
+plot(date.time, j12$Global.Reactive.Power, type = "l", ylab = "Global Reactive Power", xlab = "datetime")
+dev.off()
+##
+##
